f1b5c4d3fdc7e1b046f8dcac6d7e1fd4e6f9ea53
khushivj711 <khushi.vijay8711@gmail.com>
2024-08-11 04:04:12 +0000

    add new line to print

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,6 +1,8 @@
+//Basic java program
 import  java.lang.*;
 public class FirstProgram{
 public static void main (String[] args){
     System.out.println("Welcome to jpg coding");
+    System.out.println("This is my first program");
 }
 }

ec083547b485a09acaa12ba090b4c5d775936124
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-02 10:07:17 +0000

    literal java code

---
+++ literal.java
@@ -0,0 +1,20 @@
+public class literal {
+    public static void main(String[] args) {
+        byte age = 34; 
+        int age1 = 32;
+        char ch = 'A';
+        long ageDino = 4356435654L;
+        float f1 = 5.6f;
+        double d1 = 4.66d;
+        boolean a = true;
+        System.out.print(age);
+        String str = "Harry" ;
+        System.out.println(str);
+        System.out.println((age1));
+        System.out.println((ch));
+        System.out.println((ageDino));
+        System.out.println((f1));
+        System.out.println(d1);
+        System.out.println(a);
+    }
+}

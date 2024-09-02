2cee61fbf20536e54607e2fdf09f10266c054245
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-02 15:18:24 +0530

    variable

---
+++ 01_Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+
+    public static void main(String[] args){
+        System.out.println("The sum of three numbers is :");
+        int num1 = 6;
+        int num2 = 5;
+        int num3 = 4;
+        int sum = num1+num2+num3;
+        System.out.println(sum);
+    }
+}
+
+
